3c6636dfb092c6f4c08044767e30dfc1f5b1dd4b
hajargithub <105939774+hajargithub@users.noreply.github.com>
2023-09-20 12:16:28 +0100

    update services

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
-let firstName = "Hajar";
-let position = firstName.indexOf("H");
-console.log(position);
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+
+//         } else {
+//             entry.target.classList.remove('show');
+
+//         }
+//     });
+// });
+
+// const iconsElements = document.querySelectorAll('.skills-icons');
+// hiddenElements.forEach((el) => observer.observe(el));
+/*
+.skill-card .skills-icons img {
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}*/
+document.addEventListener('DOMContentLoaded', function() {
+    const skillCards = document.querySelectorAll('.skill-card');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    };
+
+    const cardObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    skillCards.forEach(function(card) {
+        cardObserver.observe(card);
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const skillCards = document.querySelectorAll('.experience-card');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    };
+
+    const cardObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    skillCards.forEach(function(card) {
+        cardObserver.observe(card);
+    });
+});
